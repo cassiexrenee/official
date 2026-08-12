2b25bc9acd2f994dfc26f8156b5b143ffc20497b
cassiexrenee <135995367+cassiexrenee@users.noreply.github.com>
2026-08-12 14:19:50 -0400

    Update import paths for overview components

--- src/pages/OverviewTab.tsx
+++ src/pages/OverviewTab.tsx
@@ -2,10 +2,10 @@ import React, { useState } from "react";
 import { LayoutDashboard } from "lucide-react";
 import { Player, Snapshot, PerformanceEvaluation, AllianceSettings } from "../types";
 import { apiFetch } from "../apiConfig";
-import OverviewMetricsGrid from "../components/overview/OverviewMetricsGrid";
-import OverviewAIAdvisorCard from "../components/overview/OverviewAIAdvisorCard";
-import OverviewQueueSummary from "../components/overview/OverviewQueueSummary";
-import OverviewQueueList from "../components/overview/OverviewQueueList";
+import OverviewMetricsGrid from "../components/Overview/OverviewMetricsGrid";
+import OverviewAIAdvisorCard from "../components/Overview/OverviewAIAdvisorCard";
+import OverviewQueueSummary from "../components/Overview/OverviewQueueSummary";
+import OverviewQueueList from "../components/Overview/OverviewQueueList";
 
 interface OverviewTabProps {
   players: Player[];
